91a4f33e5e2fee3812e3deab4ec1d06aedd1adbe
Anitha1905 <hkanitha5@gmail.com>
2025-03-08 08:53:07 +0530

    1st commit

---
+++ wall.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int num of interior wall,number of exterior wall;
+    float area,total cost=0;
+    scanf("%d",&number of interior walls);
+    scanf("%d",&number of exterior walls);
+    for(int i=0;i<num of interior wall;i++)
+    {
+        scanf("%f",&area);
+        total cost += area*18;
+}
+for(i=0;i<num of exterior wall;i++)
+{
+    scanf("%f",&area);
+    total cost += area*12; 
+    
+    
+    
+}
+printf("total estimation cost: %.1f INR\n",total cost);   
+}
---
+++ wash.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int N,n;
+printf("Input value");
+if(N>2000||n>0)
+{
+    printf("25 minutes");
+}
+else if(2001<=4000)
+{
+    printf("35 minutes");
+}
+else if(4000>=N)
+{
+printf("45 minutes");
+}
+else{
+    printf("overloaded");
+    printf("Invalid input");
+}
+}
